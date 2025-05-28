d44814cc183b47ecf82d4068110fa898620cf2e9
OsmanliFarid <osmanliferid686@gmail.com>
2025-05-28 14:58:46 +0400

    I added the Home component

---
+++ src/Pages/Home/index.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import Navbar from "../../Components/common/Navbar/Index";
+
+const Home = () => {
+  return (
+    <>
+      <div className="gird justify-items-center max-w-[85vw] mt-[100px] m-[auto]">
+        <div className="grid grid-cols-[260px_260px_260px_260px] justify-items-center">
+          <img
+            className="w-[231px] row-[1/2]"
+            src="https://preview.colorlib.com/theme/alime/img/bg-img/3.jpg.webp"
+            alt=""
+          />
+          <img
+            className="w-[231px] "
+            src="https://preview.colorlib.com/theme/alime/img/bg-img/4.jpg.webp"
+            alt=""
+          />
+          <img
+            className="w-[231px] "
+            src="https://preview.colorlib.com/theme/alime/img/bg-img/5.jpg.webp"
+            alt=""
+          />
+          <img
+            className="w-[231px] "
+            src="https://preview.colorlib.com/theme/alime/img/bg-img/6.jpg.webp"
+            alt=""
+          />
+          <img
+            className="w-[231px] mt-[-70px]"
+            src="https://preview.colorlib.com/theme/alime/img/bg-img/7.jpg.webp"
+            alt=""
+          />
+          <img
+            className="w-[231px] mt-[30px]"
+            src="https://preview.colorlib.com/theme/alime/img/bg-img/9.jpg.webp"
+            alt=""
+          />
+          <img
+            className="w-[231px] mt-[-80px]"
+            src="https://preview.colorlib.com/theme/alime/img/bg-img/8.jpg.webp"
+            alt=""
+          />
+          <img
+            className="w-[231px] mt-[30px]"
+            src="https://preview.colorlib.com/theme/alime/img/bg-img/10.jpg.webp"
+            alt=""
+          />
+          <img
+            className="w-[231px] mt-[-200px] h-[380px]"
+            src="https://preview.colorlib.com/theme/alime/img/bg-img/37.jpg.webp"
+            alt=""
+          />
+          <img
+            className="w-[231px] mt-[30px]"
+            src="https://preview.colorlib.com/theme/alime/img/bg-img/5.jpg.webp"
+            alt=""
+          />
+          <img
+            className="w-[493px]  col-[3/5] mt-[-217px]"
+            src="https://preview.colorlib.com/theme/alime/img/bg-img/36.jpg.webp"
+            alt=""
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
