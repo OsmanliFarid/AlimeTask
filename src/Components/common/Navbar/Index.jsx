43b7d355f83e6f89847e86244f3451be96c7826c
OsmanliFarid <osmanliferid686@gmail.com>
2025-05-28 14:58:31 +0400

    Navbar componenti

---
+++ src/Components/common/Navbar/Index.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Navbar = () => {
+  const NavbarArr = [{ id: 1, title: "Home" }];
+  return (
+    <>
+      <div className="">
+        <div className="">
+          <div className="">
+            <h1 className="font-[700] text-[30px]">
+              Alime <span className="text-red-900">.</span>
+            </h1>
+          </div>
+          <div className=""></div>
+          <div className=""></div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
