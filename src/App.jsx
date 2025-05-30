ebf562d86777276f9922d1d324cdfc93d7c888c8
OsmanliFarid <osmanliferid686@gmail.com>
2025-05-30 06:12:19 +0400

    app

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Home from "./Pages/Home";
 import { Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/common/Navbar/Index";
+import About from "./Pages/About";
 
 const App = () => {
   return (
     <>
       <Routes>
         <Route path="/" Component={Home} />
+        <Route path="/about" Component={About} />
       </Routes>
     </>
   );
