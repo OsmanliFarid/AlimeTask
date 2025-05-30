af89a5f338e9c541d6f1f8d746a76a0e442fe869
OsmanliFarid <osmanliferid686@gmail.com>
2025-05-30 06:13:08 +0400

    I added About pages

---
+++ src/Pages/About/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "../../Components/common/Navbar/Index";
+
+const About = () => {
+  return (
+    <>
+      <div className="bg-[url('/bg.png')] h-[100vh] bg-cover bg-center">
+        <Navbar />
+      </div>
+      <h1>salam</h1>
+    </>
+  );
+};
+
+export default About;
