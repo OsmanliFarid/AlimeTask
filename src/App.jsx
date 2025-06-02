e0869bdd463891f728570dace71215cf6f935a47
OsmanliFarid <osmanliferid686@gmail.com>
2025-06-02 15:59:50 +0400

    app

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,17 @@ import Home from "./Pages/Home";
 import { Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/common/Navbar/Index";
 import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import Gallery from "./Pages/Gallery";
 
 const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/" Component={Home} />
-        <Route path="/about" Component={About} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/gallery" element={<Gallery />} />
       </Routes>
     </>
   );
