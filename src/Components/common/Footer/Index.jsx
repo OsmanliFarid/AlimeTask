68bcef7bb3bc1e94fe127ea65d1a0cea35709954
OsmanliFarid <osmanliferid686@gmail.com>
2025-06-04 22:56:52 +0400

    Footer

---
+++ src/Components/common/Footer/Index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <div className="md:max-w-[90vw] sm:max-w-[90vw] max-w-[100vw] xl:max-w-[75vw] m-auto">
+        salam
+      </div>
+    </>
+  );
+};
+
+export default Footer;
