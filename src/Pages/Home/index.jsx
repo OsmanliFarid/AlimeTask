c3dd74e4cce233719c6eac5004d3fe2a622418f8
OsmanliFarid <osmanliferid686@gmail.com>
2025-06-04 22:57:50 +0400

    Home

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -3,6 +3,7 @@ import Navbar from "../../Components/common/Navbar/Index";
 import { Link } from "react-router-dom";
 import Header from "../../Components/Header";
 import { motion } from "framer-motion";
+import Footer from "../../Components/common/Footer/Index";
 const Home = () => {
   const LinkArr = [
     {
@@ -19,79 +20,79 @@ const Home = () => {
         },
         {
           src: "https://preview.colorlib.com/theme/alime/img/bg-img/4.jpg.webp",
-          col_start: "col-start-2",
-          col_end: "col-end-3",
-          row_start: "row-start-1",
-          row_end: "row-end-3",
+          col_start: "md:col-start-2 col-start-1",
+          col_end: "md:col-end-3 col-start-2",
+          row_start: "md:row-start-1 row-start-2",
+          row_end: "md:row-end-3 row-end-4",
           id: 2,
         },
         {
-          col_start: "col-start-3",
-          col_end: "col-end-4",
+          col_start: "md:col-start-3 col-start-1",
+          col_end: "md:col-end-4 col-end-3",
           id: 3,
           src: "https://preview.colorlib.com/theme/alime/img/bg-img/5.jpg.webp",
         },
         {
-          col_start: "col-start-4",
-          col_end: "col-end-5",
-          row_start: "row-start-1",
-          row_end: "row-end-3",
+          col_start: "md:col-start-4 col-start-1",
+          col_end: "md:col-end-5 col-end-2",
+          row_start: "md:row-start-1 row-start-4",
+          row_end: "md:row-end-3 row-end-6",
           src: "https://preview.colorlib.com/theme/alime/img/bg-img/6.jpg.webp",
           id: 4,
         },
         {
           col_start: "col-start-1",
           col_end: "col-end-2",
-          row_start: "row-start-2",
-          row_end: "row-end-4",
+          row_start: "md:row-start-2 row-start-6",
+          row_end: "md:row-end-4 row-end-8",
           src: "https://preview.colorlib.com/theme/alime/img/bg-img/7.jpg.webp",
           id: 5,
         },
         {
-          col_start: "col-start-2",
-          col_end: "col-end-3",
-          row_start: "row-start-3",
-          row_end: "row-end-6",
+          col_start: "md:col-start-2 col-start-1",
+          col_end: "md:col-end-3 col-end-2",
+          row_start: "md:row-start-3 row-start-8",
+          row_end: "md:row-end-6 row-end-11",
           src: "https://preview.colorlib.com/theme/alime/img/bg-img/9.jpg.webp",
           id: 6,
         },
         {
-          col_start: "col-start-3",
-          col_end: "col-end-4",
-          row_start: "row-start-2",
-          row_end: "row-end-4",
+          col_start: "md:col-start-3 col-start-1",
+          col_end: "md:col-end-4 col-end-2",
+          row_start: "md:row-start-2 row-start-11",
+          row_end: "md:row-end-4 row-end-13",
           src: "https://preview.colorlib.com/theme/alime/img/bg-img/8.jpg.webp",
           id: 7,
         },
         {
-          col_start: "col-start-4",
+          col_start: "md:col-start-4 col-start-1",
           src: "https://preview.colorlib.com/theme/alime/img/bg-img/10.jpg.webp",
-          col_end: "col-end-5",
-          row_start: "row-start-3",
-          row_end: "row-end-4",
+          col_end: "md:col-end-5 col-end-2",
+          row_start: "md:row-start-3 row-start-13",
+          row_end: "md:row-end-4 row-end-14",
           id: 8,
         },
         {
           col_start: "col-start-1",
           col_end: "col-end-2",
-          row_start: "row-start-4",
-          row_end: "row-end-7",
+          row_start: "md:row-start-4 row-start-14",
+          row_end: "md:row-end-7 row-end-17",
           src: "https://preview.colorlib.com/theme/alime/img/bg-img/37.jpg.webp",
           id: 9,
         },
         {
-          col_start: "col-start-2",
-          col_end: "col-end-3",
-          row_start: "row-start-6",
-          row_end: "row-end-7",
+          col_start: "md:col-start-2 col-start-3",
+          col_end: "md:col-end-3 col-end-2",
+          row_start: "md:row-start-6 row-start-17",
+          row_end: "md:row-end-7 row-end-18",
           src: "https://preview.colorlib.com/theme/alime/img/bg-img/5.jpg.webp",
           id: 10,
         },
         {
-          col_start: "col-start-3",
-          col_end: "col-end-5",
-          row_start: "row-start-4",
-          row_end: "row-end-7",
+          col_start: "md:col-start-3 col-start-4",
+          col_end: "md:col-end-5 col-end-2",
+          row_start: "md:row-start-4 row-start-18",
+          row_end: "md:row-end-7 row-end-21",
           src: "https://preview.colorlib.com/theme/alime/img/bg-img/36.jpg.webp",
           id: 11,
         },
@@ -190,7 +191,7 @@ const Home = () => {
         ))}
       </div>
       <div className="grid justify-items-center max-w-[75vw] mt-[50px] m-[auto]">
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        <div className="grid grid-cols-4 grid-rows-auto gap-4">
           {active.map(({ col_start, col_end, row_end, row_start, src, id }) => (
             <motion.div
               key={id}
@@ -202,11 +203,16 @@ const Home = () => {
               transition={{ duration: 0.5, delay: id * 0.1 }}
               whileHover={{ scale: 1.05 }}
             >
-              <img src={src} alt="" className="w-full h-auto object-cover" />
+              <img
+                src={src}
+                alt=""
+                className="w-full h-auto object-cover bg-red-400"
+              />
             </motion.div>
           ))}
         </div>
       </div>
+      <Footer />
     </>
   );
 };
