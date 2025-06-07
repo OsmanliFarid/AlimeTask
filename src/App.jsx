bf227365929912c89a3dda4adc9f006175a622f8
OsmanliFarid <osmanliferid686@gmail.com>
2025-06-07 15:26:46 +0400

    app

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Navbar from "./Components/common/Navbar/Index";
 import About from "./Pages/About";
 import Contact from "./Pages/Contact";
 import Gallery from "./Pages/Gallery";
+import Blog from "./Pages/Blog";
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/gallery" element={<Gallery />} />
+        <Route path="/blog" element={<Blog />} />
       </Routes>
     </>
   );
